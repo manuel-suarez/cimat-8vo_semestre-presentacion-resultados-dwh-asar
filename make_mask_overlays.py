f1ce993db819b07881bc31456568b4af4e85f00f
Arturo Suárez <manuel.suarez@cimat.mx>
2025-02-26 10:49:44 -0600

    Use segmentation pipeline

--- make_mask_overlays.py
+++ make_mask_overlays.py
@@ -9,59 +9,68 @@ Image.MAX_IMAGE_PIXELS = None
 
 home_dir = os.path.expanduser("~")
 data_dir = os.path.join(home_dir, "data", "cimat", "dataset-cimat")
-
+app_dir = os.path.join(home_dir, "data", "cimat", "ml_application")
+out_dir = os.path.join(app_dir, "out")
+res_dir = os.path.join(app_dir, "results")
+dwh_dir = os.path.join("dwh")
 os.makedirs("figures", exist_ok=True)
 
 
-def plot_image_and_mask(basepath, filename):
-    label2 = imread(os.path.join(basepath, "dwh", "png", filename), as_gray=True)
-    fname = filename.split(".")[0]
+def plot_image_and_mask(filename):
     # fname is the date, so we need to search for ASAR filename
-    asar_fname = glob.glob(os.path.join(basepath, "image_tiff", f"*{fname}*.tif"))
-    label_fname = glob.glob(os.path.join(basepath, "mask_bin", f"*{fname}*.png"))
-    if len(asar_fname) > 0 and len(label_fname) > 0:
-        image = rasterio.open(asar_fname[0]).read(1)
-        label = imread(label_fname[0], as_gray=True).astype(np.float32)
-        print("Label array: ", np.unique(label), label.shape, label.dtype)
-        # Binarize
-        binary_image = np.where(label > 0, 1, 0)
+    asar_fname = os.path.join(out_dir, filename, f"{filename}.png")
+    label_fname = os.path.join(res_dir, f"{filename}_not_wind", f"{filename}_mask.png")
+    var_fname = os.path.join(out_dir, f"{filename}", f"{filename}_var.tif")
+    dwh_fname = os.path.join(dwh_dir, "png", f"{filename}.png")
 
-        print(fname, image.shape, label.shape, binary_image.shape)
-        print(image.dtype, label.dtype, binary_image.dtype)
-        # mask = np.ma.masked_values(label, 0.0)
+    image = imread(asar_fname, as_gray=True)
+    label = imread(label_fname, as_gray=True).astype(np.float32)
+    var = imread(var_fname, as_gray=True)
+    if os.path.exists(dwh_fname):
+        dwh = imread(dwh_fname, as_gray=True)
+    print("Label array: ", np.unique(label), label.shape, label.dtype)
+    # Binarize
+    binary_image = np.where(label > 0, 1, 0)
 
-        fig, ax = plt.subplots(1, 4, figsize=(22, 6))
-        ax[0].set_title("SAR origin")
-        im0 = ax[0].imshow(image, cmap="gray")
-        fig.colorbar(im0, ax=ax[0])
+    print(fname, image.shape, label.shape, binary_image.shape)
+    print(image.dtype, label.dtype, binary_image.dtype)
+    # mask = np.ma.masked_values(label, 0.0)
 
-        ax[1].set_title("Segmentation label")
-        im1 = ax[1].imshow(label, cmap="gray", vmin=0.0, vmax=1.0, interpolation="none")
-        fig.colorbar(im1, ax=ax[1])
-        # Masked overlay
-        ax[2].set_title("Mask")
-        ax[2].imshow(image, cmap="gray")
-        mask = np.ma.masked_where(binary_image == 0, binary_image, copy=True)
-        im2 = ax[2].imshow(
-            mask,
-            cmap="viridis",
-            alpha=0.8,
-            vmin=0.0,
-            vmax=1.0,
-            interpolation="none",
-        )
-        fig.colorbar(im2, ax=ax[2])
-
-        ax[3].set_title("Mask 2")
-        ax[3].imshow(label2, cmap="gray")
-        fig.suptitle(f"{fname}, {asar_fname[0].split('/')[-1]}")
+    if os.path.exists(dwh_fname):
+        fig, ax = plt.subplots(1, 5, figsize=(26, 6))
     else:
-        fig, ax = plt.subplots(1, 1, figsize=(16, 6))
-        ax.set_title("Mask 2")
-        ax.imshow(label2, cmap="gray")
-        fig.suptitle(fname)
+        fig, ax = plt.subplots(1, 4, figsize=(22, 6))
+    ax[0].set_title("SAR origin")
+    im0 = ax[0].imshow(image, cmap="gray")
+    fig.colorbar(im0, ax=ax[0])
 
-    plt.savefig(os.path.join("figures", fname + ".png"))
+    ax[1].set_title("Segmentation label")
+    im1 = ax[1].imshow(label, cmap="gray", vmin=0.0, vmax=1.0, interpolation="none")
+    fig.colorbar(im1, ax=ax[1])
+    # Masked overlay
+    ax[2].set_title("Mask")
+    ax[2].imshow(image, cmap="gray")
+    mask = np.ma.masked_where(binary_image == 0, binary_image, copy=True)
+    im2 = ax[2].imshow(
+        mask,
+        cmap="viridis",
+        alpha=0.8,
+        vmin=0.0,
+        vmax=1.0,
+        interpolation="none",
+    )
+    fig.colorbar(im2, ax=ax[2])
+
+    ax[3].set_title("Variance")
+    ax[3].imshow(var, cmap="gray")
+
+    if os.path.exists(dwh_fname):
+        ax[4].set_title("Segmentation")
+        ax[4].imshow(dwh, cmap="gray")
+
+    fig.suptitle(f"{fname}, {asar_fname[0].split('/')[-1]}")
+
+    plt.savefig(os.path.join("figures", filename + ".png"))
     plt.close()
 
 
@@ -74,11 +83,13 @@ print("SLURM_TASK_PID: ", slurm_task_pid)
 
 # Open image according to slurm proc id
 index = int(slurm_procid) - 1
-print(os.listdir(os.path.join(data_dir, "dwh", "png")))
-print(len(os.listdir(os.path.join(data_dir, "dwh", "png"))))
-print("Index of image: ", index)
-fname = os.listdir(os.path.join(data_dir, "dwh", "png"))[index]
+files_dir = glob.glob(os.path.join(out_dir, "ASA_*"))
+# print(files_dir)
+print(len(files_dir))
+# print("Index of image: ", index)
+fname = files_dir[index]
+fname = fname.split("/")[-1]
 print("Image: ", fname)
-
-# Data
-plot_image_and_mask(data_dir, fname)
+#
+## Data
+plot_image_and_mask(fname)
